6c1dc0c30597498c0cc7681535b5c9294823270e
spaceinvader10 <54293311+spaceinvader10@users.noreply.github.com>
2023-02-01 13:41:22 -0800

    Remove method was added into numberUpdate to remove the divs before the updated number of squares are entered.

--- main.js
+++ main.js
@@ -3,30 +3,26 @@ let number = 15;
 
 let gridContainer = document.getElementsByClassName('grid-container')[0];
 
-
 let squareNumber = document.getElementById('numberOfSquaresInput');
 
-squareNumber.addEventListener('keydown',numberUpdate)
+squareNumber.addEventListener('keydown', numberUpdate);
 
-
-function numberUpdate(event){
-    //x.preventDefault();
-    if (event.key === 'Enter'){
+function numberUpdate(event) {
+    if (event.key === 'Enter') {
         console.log(squareNumber.value);
         number = squareNumber.value;
 
+        // Remove existing squares
+        while(gridContainer.firstChild){
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+        //Checks if gridContainer has a firstChild, if it does, then while that is true start removing firstChild until none exist.
+        //this is put here so it removes the previous divs before the new divs are entered in
 
-        for(let i = 0; i <= number-1 ; i++){
-        let square = document.createElement('div')
-        square.className ='grid-item '+ i; 
-        gridContainer.appendChild(square);
+        for (let i = 0; i < number; i++) {
+            square = document.createElement('div');
+            square.className = 'grid-item';
+            gridContainer.appendChild(square);
+        }
     }
-
-
-
-    
 }
-}
-
-
-
