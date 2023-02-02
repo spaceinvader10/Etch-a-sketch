55a2bba5df2597c395226fdc29fc7718fdbd1b7f
spaceinvader10 <54293311+spaceinvader10@users.noreply.github.com>
2023-02-02 15:32:00 -0800

    Will need to add color picker in the next update, right now the sketching area has some perimeter. Debating if we should use an image or the default color picker.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let square;
+
 let number = 15;
 
 let gridContainer = document.getElementsByClassName('grid-container')[0];
@@ -20,9 +20,25 @@ function numberUpdate(event) {
         //this is put here so it removes the previous divs before the new divs are entered in
 
         for (let i = 0; i < number; i++) {
-            square = document.createElement('div');
+            let square = document.createElement('div');
             square.className = 'grid-item';
             gridContainer.appendChild(square);
+
+            square.addEventListener('click', function(){
+                square.classList.toggle('hover');
+            })
         }
     }
 }
+
+// need to add mouse over squares will change the color to red, black, blue or whatever
+
+//the below code does not work because it only updates once and is outside of the numberUPdate function. Needs to be in the function so it gets called everytime the square is made
+// [...document.querySelectorAll('.grid-item')].forEach(function(item){
+//     item.addEventListener('mouseover', function(){
+//         item.classList.toggle('hover'); 
+//     })
+// })
+
+
+
